a0cfb9f1906d61f94ebd6c40f135730918e3096e
BrianGrant-Data <gra11027@byui.edu>
2022-02-15 10:15:54 -0700

    Started tutorial_CE part 3

--- scripts/tutorial_CE.py
+++ scripts/tutorial_CE.py
@@ -78,5 +78,77 @@ def t2_rescaling(): # https://www.youtube.com/watch?v=y76C3P20rwc&list=PLEsfXFp6
     cap.release()
     cv2.destroyAllWindows()
 
+def t3_record_video(
+    filename="video.mp4",
+    fps=24.0,
+    res="720p"): 
+    # https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python/ #https://www.youtube.com/watch?v=1eHQIu4r0Bc&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=5
+    '''
+    Default parameters:
+
+    filename = "video.mp4" # .mp4 and .avi are the file types focuse on here
+    fps = 24.0 # Frames per second
+    res = "720p"
+    '''
+    import numpy as np
+    import os
+    import cv2
+
+    # Set resolution for the video capture
+    # Function adapted from https://kirr.co/0l6qmh
+    def change_res(cap, width, height):
+        cap.set(3, width)
+        cap.set(4, height)
+
+    # Standard Video Dimensions Sizes
+    STD_DIMENSIONS =  {
+        "480p": (640, 480),
+        "720p": (1280, 720),
+        "1080p": (1920, 1080),
+        "4k": (3840, 2160),
+    }
+
+    # grab resolution dimensions and set video capture to it.
+    def get_dims(cap, res='1080p'):
+        width, height = STD_DIMENSIONS["480p"]
+        if res in STD_DIMENSIONS:
+            width,height = STD_DIMENSIONS[res]
+        ## change the current caputre device
+        ## to the resulting resolution
+        change_res(cap, width, height)
+        return width, height
+
+    # Video Encoding, might require additional installs
+    # Types of Codes: http://www.fourcc.org/codecs.php
+    VIDEO_TYPE = {
+        'avi': cv2.VideoWriter_fourcc(*'XVID'),
+        #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+        'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+    }
+
+    def get_video_type(filename):
+        filename, ext = os.path.splitext(filename)
+        if ext in VIDEO_TYPE:
+        return  VIDEO_TYPE[ext]
+        return VIDEO_TYPE['avi']
+
+    cap = cv2.VideoCapture(0)
+    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+
+    while True:
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# Tutorial 4 face recognition
+# Tutorial 5 overlay
+# Tutorial 6 Third party Cascades
+# Tutorial 8 Image Filters
 
 t2_rescaling()
