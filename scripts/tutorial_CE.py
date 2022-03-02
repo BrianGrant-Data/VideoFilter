2020368a96cc36c1957c3bd0ba5bdd385cb3409d
BrianGrant-Data <gra11027@byui.edu>
2022-03-01 17:37:08 -0700

    Minor changes now that PIL & pyautogui are working

--- scripts/tutorial_CE.py
+++ scripts/tutorial_CE.py
@@ -3,6 +3,11 @@
 
 import numpy as np
 import cv2
+import os
+import PIL
+import pyautogui
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def t1_webcam_display(): # https://www.youtube.com/watch?v=YY9f-6u2Q_c&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=3
     '''
@@ -78,77 +83,187 @@ def t2_rescaling(): # https://www.youtube.com/watch?v=y76C3P20rwc&list=PLEsfXFp6
     cap.release()
     cv2.destroyAllWindows()
 
-def t3_record_video(
-    filename="video.mp4",
-    fps=24.0,
-    res="720p"): 
-    # https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python/ #https://www.youtube.com/watch?v=1eHQIu4r0Bc&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=5
-    '''
-    Default parameters:
+# def t3_record_video(
+#     filename="video.mp4",
+#     fps=24.0,
+#     res="720p"): 
+#     # https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python/ #https://www.youtube.com/watch?v=1eHQIu4r0Bc&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=5
+#     '''
+#     Default parameters:
 
-    filename = "video.mp4" # .mp4 and .avi are the file types focuse on here
-    fps = 24.0 # Frames per second
-    res = "720p"
-    '''
-    import numpy as np
-    import os
-    import cv2
+#     filename = "video.mp4" # .mp4 and .avi are the file types focuse on here
+#     fps = 24.0 # Frames per second
+#     res = "720p"
+#     '''
+#     import numpy as np
+#     import os
+#     import cv2
 
-    # Set resolution for the video capture
-    # Function adapted from https://kirr.co/0l6qmh
-    def change_res(cap, width, height):
-        cap.set(3, width)
-        cap.set(4, height)
+#     # Set resolution for the video capture
+#     # Function adapted from https://kirr.co/0l6qmh
+#     def change_res(cap, width, height):
+#         cap.set(3, width)
+#         cap.set(4, height)
 
-    # Standard Video Dimensions Sizes
-    STD_DIMENSIONS =  {
-        "480p": (640, 480),
-        "720p": (1280, 720),
-        "1080p": (1920, 1080),
-        "4k": (3840, 2160),
-    }
+#     # Standard Video Dimensions Sizes
+#     STD_DIMENSIONS =  {
+#         "480p": (640, 480),
+#         "720p": (1280, 720),
+#         "1080p": (1920, 1080),
+#         "4k": (3840, 2160),
+#     }
 
-    # grab resolution dimensions and set video capture to it.
-    def get_dims(cap, res='1080p'):
-        width, height = STD_DIMENSIONS["480p"]
-        if res in STD_DIMENSIONS:
-            width,height = STD_DIMENSIONS[res]
-        ## change the current caputre device
-        ## to the resulting resolution
-        change_res(cap, width, height)
-        return width, height
+#     # grab resolution dimensions and set video capture to it.
+#     def get_dims(cap, res='1080p'):
+#         width, height = STD_DIMENSIONS["480p"]
+#         if res in STD_DIMENSIONS:
+#             width,height = STD_DIMENSIONS[res]
+#         ## change the current caputre device
+#         ## to the resulting resolution
+#         change_res(cap, width, height)
+#         return width, height
 
-    # Video Encoding, might require additional installs
-    # Types of Codes: http://www.fourcc.org/codecs.php
-    VIDEO_TYPE = {
-        'avi': cv2.VideoWriter_fourcc(*'XVID'),
-        #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-        'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-    }
+#     # Video Encoding, might require additional installs
+#     # Types of Codes: http://www.fourcc.org/codecs.php
+#     VIDEO_TYPE = {
+#         'avi': cv2.VideoWriter_fourcc(*'XVID'),
+#         #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+#         'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+#     }
 
-    def get_video_type(filename):
-        filename, ext = os.path.splitext(filename)
-        if ext in VIDEO_TYPE:
-        return  VIDEO_TYPE[ext]
-        return VIDEO_TYPE['avi']
+#     def get_video_type(filename):
+#         filename, ext = os.path.splitext(filename)
+#         if ext in VIDEO_TYPE:
+#         return  VIDEO_TYPE[ext]
+#         return VIDEO_TYPE['avi']
 
-    cap = cv2.VideoCapture(0)
-    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+#     cap = cv2.VideoCapture(0)
+#     out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
 
-    while True:
-        ret, frame = cap.read()
-        out.write(frame)
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+#     while True:
+#         ret, frame = cap.read()
+#         out.write(frame)
+#         cv2.imshow('frame',frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
 
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+#     cap.release()
+#     out.release()
+#     cv2.destroyAllWindows()
 
 # Tutorial 4 face recognition
+def t4_face_cascadeClassifier():
+    '''
+    https://www.youtube.com/watch?v=PmZ29Vta7Vc&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=7
+    '''
+    import numpy as np
+    import cv2
+
+    cascade_path = "C:/Users/bag20/projects/video_filter/.venv/lib/site-packages/cv2/data/" # to find your openCV package location type 'python' in the terinal and then type 'print(cv2.__file__)' in the terminal. Exit py typing 'exit()'.
+    face_cascade = cv2.CascadeClassifier(cascade_path+'haarcascade_frontalface_alt2.xml')
+    eye_cascade = cv2.CascadeClassifier(cascade_path+'haarcascade_eye.xml')
+    smile_cascade = cv2.CascadeClassifier(cascade_path+'yyhaarcascade_smile.xml')
+
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            #print(x,y,w,h)
+            roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+            roi_color = frame[y:y+h, x:x+w]
+
+            # Draw Bounding Box
+            color = (255, 0, 0) #BGR 0-255 
+            stroke = 2
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+            #subitems = smile_cascade.detectMultiScale(roi_gray)
+            #for (ex,ey,ew,eh) in subitems:
+            #	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+def t4a_face_recognizer():
+    '''
+    https://www.youtube.com/watch?v=PmZ29Vta7Vc&list=PLEsfXFp6DpzRyxnU-vfs3vk-61Wpt7bOS&index=7
+    '''
+    import numpy as np
+    import cv2
+    import pickle
+
+    cascade_path = "C:/Users/bag20/projects/video_filter/.venv/lib/site-packages/cv2/data/" # to find your openCV package location type 'python' in the terinal and then type 'print(cv2.__file__)' in the terminal. Exit py typing 'exit()'.
+    face_cascade = cv2.CascadeClassifier(cascade_path+'haarcascade_frontalface_alt2.xml')
+    eye_cascade = cv2.CascadeClassifier(cascade_path+'haarcascade_eye.xml')
+    smile_cascade = cv2.CascadeClassifier(cascade_path+'yyhaarcascade_smile.xml')
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("./recognizers/face-trainner.yml")
+
+    labels = {"person_name": 1}
+    with open("pickles/face-labels.pickle", 'rb') as f:
+        og_labels = pickle.load(f)
+        labels = {v:k for k,v in og_labels.items()}
+
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            #print(x,y,w,h)
+            roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+            roi_color = frame[y:y+h, x:x+w]
+
+            #image recognizer. You could use a deep learning model to predict as well: keras tensorflow pytorch scikit learn
+            id_, conf = recognizer.predict(roi_gray)
+            if conf>=4 and conf <= 85:
+                #print(5: #id_)
+                #print(labels[id_])
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                name = labels[id_]
+                color = (255, 255, 255)
+                stroke = 2
+                cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+            img_item = "7.png"
+            cv2.imwrite(img_item, roi_color)
+
+            # Draw Bounding Box
+            color = (255, 0, 0) #BGR 0-255 
+            stroke = 2
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+            #subitems = smile_cascade.detectMultiScale(roi_gray)
+            #for (ex,ey,ew,eh) in subitems:
+            #	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
 # Tutorial 5 overlay
 # Tutorial 6 Third party Cascades
 # Tutorial 8 Image Filters
 
-t2_rescaling()
+t4_face_cascadeClassifier()
--- scripts/tutorial_LCG.py
+++ scripts/tutorial_LCG.py
@@ -1,59 +1,108 @@
 # Learn Code By Gaming's OpenCV Tutorial
-#%% Import Libraries
+
+# Import Libraries
 import cv2 as cv
 import numpy as np
 import urllib
 import io
+from time import time
+from PIL import Image, ImageGrab
+import pyautogui
 
-#%% Load Test Image
-## Image was downloaded from here 'https://raw.githubusercontent.com/opencv/opencv/4.x/samples/data/lena.jpg'
 
-#path = "lena.jpg"
-img_needle = cv.imread("../input/lena.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be looking for
-img_haystack = cv.imread("../input/lena_screenshot.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be searching for the small image
+def tutorial1():
+    # Load Test Image
+    ## Image was downloaded from here 'https://raw.githubusercontent.com/opencv/opencv/4.x/samples/data/lena.jpg'
 
-result = cv.matchTemplate(img_haystack, img_needle, cv.TM_CCOEFF_NORMED) # this returns an image where the white points are where the image we're looking for overlaps the background searched
+    #path = "lena.jpg"
+    img_needle = cv.imread("../input/lena.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be looking for
+    img_haystack = cv.imread("../input/lena_screenshot.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be searching for the small image
 
-#%% Show Results
-cv.imshow('Result', result)
-cv.waitKey()
-cv.destroyAllWindows()
-    
-#%% Find Location of Match
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) # Returns the best match's position
-print('Best match top left position: %s' % str(max_loc))
-print('Best match confidence: %s' % max_val)
+    result = cv.matchTemplate(img_haystack, img_needle, cv.TM_CCOEFF_NORMED) # this returns an image where the white points are where the image we're looking for overlaps the background searched
 
-#%% Basic Thresholding 
-threshold = 0.8 # Selects what confidence value we're hoping the match will exceed
-if max_val >= threshold:
-    print('Needle was found.')
-else:
-    print('Needle was not found.')
-
-#%% Basic Bounding Box 
-threshold = 0.8 # Selects what confidence value we're hoping the match will exceed
-if max_val >= threshold:    
-    # Get needle image dimensions
-    needle_w = img_needle.shape[1]
-    needle_h = img_needle.shape[0]
-
-    # Set Bounding Box Parameters
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-
-    cv.rectangle(img_haystack, top_left, bottom_right,
-        color=(0,255,0), thickness=2, lineType=cv.LINE_4)
-
-    # Display Image with Bounding Box
-    cv.imshow('Result', img_haystack)
+    # Show Results
+    cv.imshow('Result', result)
     cv.waitKey()
     cv.destroyAllWindows()
-else:
-    print('Conf. Threshold is too low to draw relevant bounding box.')
+    
+def tutorial2():
+    # Load Test Image
+    ## Image was downloaded from here 'https://raw.githubusercontent.com/opencv/opencv/4.x/samples/data/lena.jpg'
 
-# %% Saving an Image as an Output
-path = 'C:/Users/bag20/projects/video_filter/output/gitignore'
-cv.imwrite(path + 'result.jpg', img_haystack)
+    #path = "lena.jpg"
+    img_needle = cv.imread("../input/lena.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be looking for
+    img_haystack = cv.imread("../input/lena_screenshot.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be searching for the small image
 
-# %%
+    result = cv.matchTemplate(img_haystack, img_needle, cv.TM_CCOEFF_NORMED) # this returns an image where the white points are where the image we're looking for overlaps the background searched
+
+    #%% Find Location of Match
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) # Returns the best match's position
+    print('Best match top left position: %s' % str(max_loc))
+    print('Best match confidence: %s' % max_val)
+
+    #%% Basic Thresholding 
+    threshold = 0.8 # Selects what confidence value we're hoping the match will exceed
+    if max_val >= threshold:
+        print('Needle was found.')
+    else:
+        print('Needle was not found.')
+
+    #%% Basic Bounding Box 
+    threshold = 0.8 # Selects what confidence value we're hoping the match will exceed
+    if max_val >= threshold:    
+        # Get needle image dimensions
+        needle_w = img_needle.shape[1]
+        needle_h = img_needle.shape[0]
+
+        # Set Bounding Box Parameters
+        top_left = max_loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+
+        cv.rectangle(img_haystack, top_left, bottom_right,
+            color=(0,255,0), thickness=2, lineType=cv.LINE_4)
+
+        # Display Image with Bounding Box
+        cv.imshow('Result', img_haystack)
+        cv.waitKey()
+        cv.destroyAllWindows()
+    else:
+        print('Conf. Threshold is too low to draw relevant bounding box.')
+
+def tutorial2a():
+    # Load Test Image
+    ## Image was downloaded from here 'https://raw.githubusercontent.com/opencv/opencv/4.x/samples/data/lena.jpg'
+
+    #path = "lena.jpg"
+    img_needle = cv.imread("../input/lena.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be looking for
+    img_haystack = cv.imread("../input/lena_screenshot.jpg", cv.IMREAD_UNCHANGED) # this imports the image we'll be searching for the small image
+
+    result = cv.matchTemplate(img_haystack, img_needle, cv.TM_CCOEFF_NORMED) # this returns an image where the white points are where the image we're looking for overlaps the background searched
+
+    # Saving an Image as an Output
+    path = 'C:/Users/bag20/projects/video_filter/output/gitignore'
+    cv.imwrite(path + 'result.jpg', img_haystack)
+
+def tutorial4():
+    # %% displaying a screen
+    loop_time = time()
+    while(True):
+        # screenshot = pyautogui.screenshot() # or screenshot = ImageGrab.grab()
+        screenshot = ImageGrab.grab()
+        screenshot = np.array(screenshot)
+        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR) # np.array changes the color scheme to RGB but cv2 uses BGR and the colors will look weird if you don't switch it
+
+        cv.imshow('Computer Vision', screenshot)
+
+        # Display frames per second (FPS)
+        print('FPS {}'.format(round(1/(time() - loop_time)), 2)) # goal is 24 fps minimum. time()-loop_time returns the seconds per frame (SPF). 1/SPF returns the frames per second (FPS)
+        loop_time = time()
+
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            # screenshot.release()
+            cv.destroyAllWindows()
+            break
+
+    # When everything done, release the capture
+    
+
+tutorial4()
