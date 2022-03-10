3459dbe9928a8f31c5a49fafb261b96c04723390
BrianGrant-Data <gra11027@byui.edu>
2022-03-10 11:54:45 -0700

    The start of a class and tut4 args have been added

--- scripts/tutorial_LCG.py
+++ scripts/tutorial_LCG.py
@@ -5,10 +5,105 @@ import cv2 as cv
 import numpy as np
 import urllib
 import io
+import sys
 from time import time
 from PIL import Image, ImageGrab
 import pyautogui
+import win32gui, win32ui, win32con
 
+class WindowCapture:
+
+    # properties
+    w = 0
+    h = 0
+    hwnd = None
+    cropped_x = 0
+    cropped_y = 0
+    offset_x = 0
+    offset_y = 0
+
+    # constructor
+    def __init__(self, window_name=None):
+        # find the handle for the window we want to capture
+        if window_name is None:
+            self.hwnd = win32gui.GetDesktopWindow()
+        else:
+            self.hwnd = win32gui.FindWindow(None, window_name)
+            if not self.hwnd:
+                raise Exception('Window not found: {}'.format(window_name))
+
+        # get the window size
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+
+        # account for the window border and titlebar and cut them off
+        border_pixels = 8
+        titlebar_pixels = 30
+        self.w = self.w - (border_pixels * 2)
+        self.h = self.h - titlebar_pixels - border_pixels
+        self.cropped_x = border_pixels
+        self.cropped_y = titlebar_pixels
+
+        # set the cropped coordinates offset so we can translate screenshot
+        # images into actual screen positions
+        self.offset_x = window_rect[0] + self.cropped_x
+        self.offset_y = window_rect[1] + self.cropped_y
+
+    def get_screenshot(self):
+
+        # get the window image data
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        # convert the raw data into a format opencv can read
+        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # free resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type() 
+        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+        img = img[...,:3]
+
+        # make image C_CONTIGUOUS to avoid errors that look like:
+        #   File ... in draw_rectangles
+        #   TypeError: an integer is required (got type tuple)
+        # see the discussion here:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    # find the name of the window you're interested in.
+    # once you have it, update window_capture()
+    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
+    @staticmethod
+    def list_window_names():
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
+
+    # translate a pixel position on a screenshot image to a pixel position on the screen.
+    # pos = (x, y)
+    # WARNING: if you move the window being captured after execution is started, this will
+    # return incorrect coordinates, because the window position is only calculated in
+    # the __init__ constructor.
+    def get_screen_position(self, pos):
+        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
 
 def tutorial1():
     # Load Test Image
@@ -82,12 +177,23 @@ def tutorial2a():
     path = 'C:/Users/bag20/projects/video_filter/output/gitignore'
     cv.imwrite(path + 'result.jpg', img_haystack)
 
-def tutorial4():
+def tutorial4(method = ["pyauto", "ImageGrab", "wincap"], screen_name = "tutorial_LCG.py - video_filter - Visual Studio Code"):
+    methods = ["pyauto", "ImageGrab", "wincap"]
+    if method not in methods:
+        sys.exit("Method not in methods")
+    
     # %% displaying a screen
     loop_time = time()
     while(True):
-        # screenshot = pyautogui.screenshot() # or screenshot = ImageGrab.grab()
-        screenshot = ImageGrab.grab()
+        if method == "pyauto":
+            screenshot = pyautogui.screenshot() # or screenshot = ImageGrab.grab()
+        elif method == "ImageGrab":
+            screenshot = ImageGrab.grab()
+        elif method == "wincap":
+            # initialize the WindowCapture class
+            wincap = WindowCapture(str(screen_name))
+            screenshot = wincap.get_screenshot()
+            
         screenshot = np.array(screenshot)
         screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR) # np.array changes the color scheme to RGB but cv2 uses BGR and the colors will look weird if you don't switch it
 
@@ -104,5 +210,6 @@ def tutorial4():
 
     # When everything done, release the capture
     
-
-tutorial4()
+tutorial4("pyauto")
+# tutorial4("wincap", "Real-time Object Detection - OpenCV Object Detection in Games #5 - YouTube - Google Chrome")
+# WindowCapture.list_window_names()
