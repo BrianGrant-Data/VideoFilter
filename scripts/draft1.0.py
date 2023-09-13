d2d03d3267e266718700a664c2121778eb71e4d3
BrianGrant-Data <gra11027@byui.edu>
2023-09-13 05:12:52 -0600

    Potential big changes
    Apparently,  these were never pushed at the close of the project. See lines 10, 32, and 35-37

--- scripts/draft1.0.py
+++ scripts/draft1.0.py
@@ -7,7 +7,6 @@ import mss
 import urllib
 import io
 from PIL import Image, ImageGrab
-import pyautogui
 
 # Cascade Paths
 cascade_path = "C:/Users/bag20/projects/video_filter/.venv/lib/site-packages/cv2/data/" # to find your openCV package location type 'python' in the terinal and then type 'print(cv2.__file__)' in the terminal. Exit py typing 'exit()'.
@@ -19,22 +18,27 @@ face_cascade = cv.CascadeClassifier(cascade_path+'haarcascade_frontalface_alt2.x
 
 # Main
 def main(monitor_number):
-    # display screen
+    
     while(True):
         start = time.time()
 
+<<<<<<< Updated upstream
         screenshot = pyautogui.screenshot() # or screenshot = ImageGrab.grab()
+=======
+        # Capture the Screen
+        screenshot = ImageGrab.grab()
+
+        # Prep the image
+>>>>>>> Stashed changes
         screenshot = np.array(screenshot)
         screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR) # np.array changes the color scheme to RGB but cv2 uses BGR and the colors will look weird if you don't switch it
-
-        # Capture frame-by-frame
         gray  = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+        # Pass to classifier
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5) # Returns the regions of interest
   
+        # Alter the region of interest
         for (x, y, w, h) in faces:
-            #print(x,y,w,h)
-            roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-            roi_color = screenshot[y:y+h, x:x+w]
 
             # Draw Bounding Box
             color = (255, 0, 0) #BGR 0-255 
@@ -43,21 +47,26 @@ def main(monitor_number):
             end_cord_y = y + h
             cv.rectangle(screenshot, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
+<<<<<<< Updated upstream
 
         # Display Frames Per Second (FPS)
+=======
+        # Display Time
+>>>>>>> Stashed changes
         end = time.time()
         totalTime = end - start
-
         fps = 1 / totalTime
-
         cv.putText(screenshot, f'FPS: {int(fps)}', (20,70), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-        
+
+        # Display image
+        screenshot = cv.resize(screenshot, (1280, 720)) # Resize image # altered for first screen's aspect ration
         cv.imshow('Computer Vision', screenshot)
         
-    # exit window
+        # exit window
         if cv.waitKey(20) & 0xFF == ord('q'):
             cv.destroyAllWindows()
             break
+        
     # when everything is done, release the capture
     screenshot.release()
     
